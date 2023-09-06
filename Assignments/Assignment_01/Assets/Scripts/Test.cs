0b2e7bea7c2264288a9b5b45b867c6c4ad269816
BennetYrWang <107472601+BennetYrWang@users.noreply.github.com>
2023-09-06 09:53:36 -0400

    Update Test.cs

--- Assignments/Assignment_01/Assets/Scripts/Test.cs
+++ Assignments/Assignment_01/Assets/Scripts/Test.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -14,5 +15,14 @@ public class Test : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+public class Bennet : MonoBehaviour
+{
+    //this class is for nothing
+    private class Wang : Bennet
+    {
+
     }
 }
