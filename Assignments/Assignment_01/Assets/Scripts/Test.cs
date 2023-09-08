619801e9e60df62cd8661c702b2989663469d740
BennetYrWang <107472601+BennetYrWang@users.noreply.github.com>
2023-09-08 09:52:12 -0400

    something

--- Assignments/Assignment_01/Assets/Scripts/Test.cs
+++ Assignments/Assignment_01/Assets/Scripts/Test.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Test : MonoBehaviour
 {
+    public float rot_speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +17,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-}
-
-public class Bennet : MonoBehaviour
-{
-    //this class is for nothing
-    private class Wang : Bennet
-    {
-
+        transform.Rotate(0, rot_speed * Time.deltaTime, 0);
     }
 }
