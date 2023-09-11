723a42861d7f6c88e3321b25af09ea444c4ff0bc
Ashley Shi <130475141+Aslnnn0111@users.noreply.github.com>
2023-09-11 14:14:38 -0400

    assignment1_push

--- Assignment/GameDev_Ashley_assignment1/Assets/script/test.cs
+++ Assignment/GameDev_Ashley_assignment1/Assets/script/test.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
-{t
+public class test: MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
