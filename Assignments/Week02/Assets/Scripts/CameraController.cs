6c8fdfbb2934ed0c96d97a8efb6c4e8248881442
mhosster <46171808+HHOOSSPPEE@users.noreply.github.com>
2023-09-26 21:08:53 -0400

    Movement Demo

---
+++ Assignments/Week02/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = .125f;
+    public Vector3 offset;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    public void LateUpdate()
+    {
+        Transform currentTarget = target;
+
+        Vector3 desiredPosition = currentTarget.position + offset;
+
+        Vector3 smoothedPostion = Vector3.SmoothDamp(
+            transform.position,
+            desiredPosition,
+            ref _velocity,
+            smoothSpeed);
+
+        transform.position = smoothedPostion;
+    }
+}
