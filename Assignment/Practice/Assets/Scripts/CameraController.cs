5fbd65775162dd902b387d44aa1dce3d85547505
Shengyuezzz <sz3825@nyu.edu>
2023-09-28 21:51:09 -0400

    twoooooooo

--- Assignment/Practice/Assets/Scripts/CameraController.cs
+++ Assignment/Practice/Assets/Scripts/CameraController.cs
@@ -8,15 +8,23 @@ public class CameraController : MonoBehaviour
     public float smoothSpeed = .125f;
     public Vector3 offset;
     private Vector3 _velocity = Vector3.zero;
+    public Vector3 minValue, maxValue;
     // Start is called before the first frame update
 
     public void LateUpdate()
     {
         Transform currentTarget = target;
         Vector3 desiredPosition = currentTarget.position + offset;
-
+        /*Vector3 boundPosition = new Vector3(Mathf.Clamp(desiredPosition.x, minValue.x, maxValue.x),
+                                            Mathf.Clamp(desiredPosition.y, minValue.y, maxValue.y),
+                                            Mathf.Clamp(desiredPosition.z, minValue.z, maxValue.z));*/
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, smoothSpeed);
 
         transform.position = smoothedPosition;
     }
+    
+
+
+        
 }
+
