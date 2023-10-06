43fc6efa0a3b7db5df00565fccdb840e95d442d9
jlynli <130990818+jlynli@users.noreply.github.com>
2023-10-06 14:53:02 -0400

    from class demo

---
+++ assignments/week3/Assets/scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+    public void SwitchScene(string newScene)
+    {
+        StartCoroutine(SceneWait(0.3f, newScene));
+        
+    }
+
+    IEnumerator SceneWait(float waitTime, string newScene)
+    {
+        //everthing here right away
+        yield return new WaitForSeconds(waitTime);
+
+        //everything here after a wait
+        SceneManager.LoadScene(newScene);
+
+    }
+}
