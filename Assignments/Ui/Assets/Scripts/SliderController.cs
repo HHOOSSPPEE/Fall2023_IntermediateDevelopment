aaee470f7f4f589dbf1cdfb668cf5b391c6998f4
oirbeboq <revamqq@gmail.com>
2023-10-11 13:53:53 -0400

    Sliders Update

--- Assignments/Ui/Assets/Scripts/SliderController.cs
+++ Assignments/Ui/Assets/Scripts/SliderController.cs
@@ -5,12 +5,20 @@ using TMPro;
 
 public class UISlideController : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI sliderText = null;
-    [SerializeField] private float maxSliderAmount = 100f;
+    [SerializeField] private TextMeshProUGUI soundText = null;
+    [SerializeField] private TextMeshProUGUI musicText = null;
+    [SerializeField] private float soundmaxSliderAmount = 100f;
+    [SerializeField] private float musicmaxSliderAmount = 100f;
 
-    public void sliderChange (float value)
+    public void soundsliderChange (float value)
     {
-        float localValue = value * maxSliderAmount;
-        sliderText.text = localValue.ToString("0");
+        float localValue = value * soundmaxSliderAmount;
+        soundText.text = localValue.ToString("0");
+    }
+
+    public void MusicsliderChange(float value)
+    {
+        float localValue = value * musicmaxSliderAmount;
+        musicText.text = localValue.ToString("0");
     }
 }
