aab0b991141388f8b62820f783f58a84981a44df
Ashley Shi <130475141+Aslnnn0111@users.noreply.github.com>
2023-10-13 10:05:25 -0400

    Assignment3_final_push

---
+++ Assignment/Assignment3/Assets/script/CursorChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
